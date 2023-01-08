2ff450493ff3ba5935c795f3f4b9607ae2fd1ae7
timmy-feng <timf01089@gmail.com>
2023-01-07 23:03:37 -0600

    switched to bcryptjs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const debug = require('debug')('server:server');
